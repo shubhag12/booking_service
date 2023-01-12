4f49ef4902e242868dc4e16198a0eb4e13c1bfa5
shubh agrwal <shubhagarwal123123@gmail.com>
2023-01-12 16:12:40 +0530

    added axios feature

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,6 +1,6 @@
 const { BookingRepository } = require("../repository/index");
 const axios = require("axios");
-const { FLIGHT_SERVICE_PATH } = require('../config/serverConfig');
+const { FLIGHT_SERVICE_PATH } = require("../config/serverConfig");
 class BookingService {
   constructor() {
     this.BookingRepository = new BookingRepository();
@@ -8,11 +8,25 @@ class BookingService {
   async createBooking(data) {
     try {
       const flightId = data.flightId;
-      let getFlightRequestUrl=`${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`
-     
-      const flight=await axios.get(getFlightRequestUrl);
-      
-      return flight.data.data;
+      const getFlightRequestUrl = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
+
+      const response = await axios.get(getFlightRequestUrl);
+      const flightData = response.data.data;
+      let priceOfTheFlight = flightData.price;
+      if (data.noOfSeats > flightData.totalSeats) {
+        console.log("insufficent seats");
+        throw { error };
+      }
+
+      const totalCost = priceOfTheFlight * data.noOfSeats;
+
+      const bookingPayload = { ...data, totalCost };
+
+      const booking = await this.BookingRepository.create(bookingPayload);
+      const updateFlightRequestUrl=`${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`;
+      await axios.patch(updateFlightRequestUrl,{totalSeats:flightData.totalSeats-booking.noOfSeats})
+      const finalbooking=await this.BookingRepository.update(booking.id,{status:"Booked"})
+      return finalbooking;
     } catch (error) {
       console.log("service");
       throw { error };
